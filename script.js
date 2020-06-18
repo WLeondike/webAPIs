fa9443fab17bbdda70192d2f53122c21d6d2f4ce
WLeondike <billyleondike@gmail.com>
2020-06-17 22:24:15 -0400

    updated styles, added JavaScript functions

--- script.js
+++ script.js
@@ -1,46 +1,67 @@
+// TODOs:
+// ✓ The first view of the application displays a button used to start the quiz.
+// ✓ Clicking the 'Start Quiz' button displays a series of questions.
+// ✓ Once the quiz begins, a countdown timer starts.
+// ✓ If a question is answered incorrectly, additional time is subtracted from the timer.
+// ✓ The timer ends when all questions have been answered or the timer reaches 0.
+// ✓ After the game ends, the user can save their initials and score to a highscores view using local storage.
+// ✓ Repository contains quality README with description, screenshot, link to deployed application.
+
+//GLOBALS
 var containerEl = document.querySelector(".container");
 var timerDisplay = document.querySelector(".timer");
 var startText = document.createElement("h1");
 var startBtn = document.createElement("button");
 var questionText = document.createElement("p");
-var timer = 5;
-var index = 0;
+var hr = document.createElement("hr"); document.body.appendChild(hr);
+var timer = 50;
+var qIndex = 0;
 var questions = [
   {
-    title: "Example Question 1:",
-    choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+    title:"",
+    choices:"",
+    answer:"",
+  },
+  {
+    title: "What tag is used to define an unordered list that is bulleted?",
+    choices: ["<li>", "<ol>", "<div>", "<ul>"],
+    answer: "<ul>"
+  },
+  {
+    title: "What tag is used to define a hyperlink, or a link to another page?",
+    choices: ["<em>", "<a>", "<strong>", "<script>"],
+    answer: "<a>"
+  },
+  {
+    title: "Which of these are not a programming language?",
+    choices: ["Python", "Java", "Ruby", "PHD"],
+    answer: "PHD"
+  },
+  {
+    title: "Where do you store your css stylesheet link?",
+    choices: ["<head>", "<body>", "<div>", "<html>"],
+    answer: "<head>"
+  },
+  {
+    title: "What tag is used to store your JavaScript source/code?",
+    choices: ["<img>", "<link>", "<script>", "<title>"],
+    answer: "<script>"
+  },
+  {
+    title: "Which of these are not a semantic tag?",
+    choices: ["<article>", "<div>", "<footer>", "<nav>"],
     answer: "Choice 2"
   },
   {
-    title: "Example Question 2:",
-    choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-    answer: "Choice 3"
-  },
-  {
-    title: "Example Question 3:",
-    choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-    answer: "Choice 1"
-  },
-  {
-    title: "Example Question 4:",
-    choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-    answer: "Choice 4"
-  },
-  {
-    title: "Example Question 5:",
-    choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-    answer: "Choice 4"
-  },
-  {
-    title: "Example Question 6:",
-    choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-    answer: "Choice 2"
+    title:"Still a work in progress! Will update soon🧑🏻‍🔧 ",
+    choices:"",
+    answer:"",
   }
 ];
 
 //Content for home page
 function openingPage() {
-  startText.textContent = "Here is a little quiz about coding!";
+  startText.textContent = "Welcome to my code quiz!";
   startBtn.textContent = "Start Quiz";
   containerEl.appendChild(startText);
   containerEl.appendChild(startBtn);
@@ -55,39 +76,59 @@ function startQuiz() {
 //function for the timer
 function showTimer() {
   timerDisplay.textContent = timer;
+
   var timeInterval = setInterval(function () {
     timer--;
     timerDisplay.textContent = timer;
+
     if (timer === 0) {
-      clearInterval(timeInterval)
+      clearInterval(timeInterval);
     }
-  }, 1000)
+  }, 1000);
 };
 
 //function to handle next question
 function nextQuestion() {
-  var currentQuestion = questions[index];
+  var currentQuestion = questions[qIndex];
   containerEl.textContent = "";
   questionText.textContent = currentQuestion.title;
   containerEl.appendChild(questionText);
   var answersDiv = document.createElement("div");
+
   for (let i = 0; i < currentQuestion.choices.length; i++) {
     var answerBtn = document.createElement("button");
     answerBtn.classList.add("choiceBtn");
     answerBtn.textContent = currentQuestion.choices[i];
     answersDiv.appendChild(answerBtn);
   }
+
   containerEl.appendChild(answersDiv);
+
 };
 
-function checkAnswer(event) {
-  if (event.target.matches(".choiceBtn")) {
-    index++;
-    nextQuestion()
+function checkAnswer(answerBtn) {
+  if (event.target.textContent === questions[qIndex].answer) {
+    qIndex++;
+    nextQuestion();
+    writeMessage("Correct!");
+  } else {
+    qIndex++;
+    timer -= 5;
+    nextQuestion();
+    writeMessage("Incorrect!");
   }
+}
+
+
+function writeMessage(message) {
+  var grade = document.createElement("p");
+  grade.textContent = message;
+  containerEl.append(grade);
+  var resultTimer = setTimeout(function () {
+    grade.textContent = "";
+  }, 1000)
 }
 
 startBtn.addEventListener("click", startQuiz);
 document.addEventListener("click", checkAnswer);
 openingPage();
-
